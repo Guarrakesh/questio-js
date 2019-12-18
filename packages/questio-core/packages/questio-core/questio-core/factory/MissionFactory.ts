ad1f224df4a0bc9af713289b41013fa2e18adbec
Dario Guarracino <>
2019-12-18 20:48:14 +0100

    Init lerna

--- packages/questio-core/packages/questio-core/questio-core/factory/MissionFactory.ts
+++
@@ -1,5 +0,0 @@
-import {Mission, MissionAttributes} from '../Mission';
-
-export function createMission<M extends Mission>(m: { new(config: MissionAttributes): M}, config: MissionAttributes) {
-    const mission = new m(config);
-}
