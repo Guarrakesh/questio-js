3ff4e38d71592879a84ab0742997fa5120e76e4e
Dario Guarracino <>
2019-12-18 20:45:29 +0100

    Init lerna

---
+++ packages/questio-core/packages/questio-core/questio-core/messengers/WebHookMessenger.ts
@@ -0,0 +1,8 @@
+import {CompletedMissionPayload, CompletedQuestPayload, CompletedTaskPayload} from '../interfaces/IMessageProtocol';
+
+export type callbackMessengerOptions = {
+  sendCompletedMission: (payload: CompletedMissionPayload) => void,
+  sendCompletedTask: (payload: CompletedTaskPayload) => void,
+  sendCompletedQuest: (payload: CompletedQuestPayload) => void,
+
+}
